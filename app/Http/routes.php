0a3fc6c7161e24574da121b884f0cc3dc1918132
Rui Gomes <ruigomeseu@gmail.com>
2015-02-01 22:09:47 +0000

    Added default route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,10 @@
 <?php
 
+
+Route::get('/', function () {
+    echo 'vimg.co is best host confirmed by cmat confirmer';
+});
+
 Route::post('/images', 'ImagesController@store');
 
 Route::get('{hashId}', ['as' => 'images.show', 'uses' => 'ImagesController@show']);
